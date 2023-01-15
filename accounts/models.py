c1aa78338d41a2593566176fc5144a2d5d95708b
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-01-15 18:17:50 +0100

    feat: create User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=15, blank=False, null=False, unique=True)
+    email = models.EmailField(
+        max_length=255, blank=False, null=False, unique=True)
+    first_name = models.CharField(max_length=255, blank=False, null=True)
+    last_name = models.CharField(max_length=255, blank=False, null=True)
+    description = models.CharField(max_length=100, default='Users desc.')
+    city = models.CharField(max_length=100, default='Unknown city')
+    phoneNumber = models.IntegerField(default=555666333)
+    bio = models.CharField(max_length=225, default='Users bio')
+    avatar = models.ImageField(null=True, default="default-avatar")
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
