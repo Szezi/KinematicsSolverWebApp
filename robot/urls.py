160d9c055ce6d69149de77b5af74540ed2d3f239
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-01-18 20:50:59 +0100

    feat: add Projects CRUD urls

--- robot/urls.py
+++ robot/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from django.conf import settings
-from .views import DashboardView
+from .views import DashboardView, ProjectCreate, ProjectDelete, ProjectUpdate, ProjectDetail
 
 urlpatterns = [
     path('dashboard', DashboardView.as_view(), name='dashboard'),
+
+    path('project-create/', ProjectCreate.as_view(), name='project-create'),
+    path('project-detail/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
+    path('project-update/<int:pk>/', ProjectUpdate.as_view(), name='project-update'),
+    path('project-delete/<int:pk>/', ProjectDelete.as_view(), name='project-delete')
 ]
