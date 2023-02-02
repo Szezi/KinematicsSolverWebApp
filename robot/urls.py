4726c52210db46cffcdc4b1f92d665ecadee6649
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-02-02 20:36:19 +0100

    feat: add CRUD urls for forward and inverse kinematics

--- robot/urls.py
+++ robot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.conf import settings
-from .views import DashboardView, ProjectCreate, ProjectDelete, ProjectUpdate, ProjectDetail, RobotCreate, RobotDelete, RobotUpdate, RobotDetail
+from .views import DashboardView, ProjectCreate, ProjectDelete, ProjectUpdate, ProjectDetail, RobotCreate, RobotDelete, RobotUpdate, RobotDetail, FkCreate, FkUpdate, IkCreate, IkUpdate
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -15,5 +15,12 @@ urlpatterns = [
     path('robot-detail/<int:pk>/', RobotDetail.as_view(), name='robot-detail'),
     path('robot-update/<int:pk>/', RobotUpdate.as_view(), name='robot-update'),
     path('robot-delete/<int:pk>/', RobotDelete.as_view(), name='robot-delete'),
+
+    path('fk-create/', FkCreate.as_view(), name='fk-create'),
+    path('fk-update/<int:pk>/', FkUpdate.as_view(), name='fk-update'),
+
+    path('ik-create/', IkCreate.as_view(), name='ik-create'),
+    path('ik-update/<int:pk>/', IkUpdate.as_view(), name='ik-update'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
