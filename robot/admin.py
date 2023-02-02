6025a809a15b99dbda6762a89e75c6b65d20771c
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-02-02 20:35:21 +0100

    feat: register forward and inverse kinematics models

--- robot/admin.py
+++ robot/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Project, Robot
+from .models import Project, Robot, ForwardKinematics, InverseKinematics
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Robot)
+admin.site.register(ForwardKinematics)
+admin.site.register(InverseKinematics)
 
