cd93c554e3d0d707b170d8774b289ede8af8925c
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2023-02-02 20:47:07 +0100

    feat: redirect from home to dashboard if user is logged in

--- core/views.py
+++ core/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "core/home.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return super(HomeView, self).dispatch(request, *args, **kwargs)
